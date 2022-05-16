bf6ed1593eb0d2409bd335623d36cace4d393dd8
Rob Cameron <rob.cameron@fastmail.com>
2022-05-16 15:40:53 -0700

    Refactor setup, track key presses in a buffer

--- main.lua
+++ main.lua
@@ -20,7 +20,7 @@ if not config then
 end
 
 function setupMenu()
-  local menu = pd.getSystemMenu()
+  local menu <const> = pd.getSystemMenu()
   menu:addCheckmarkMenuItem('popups', config.showDescriptions, function()
     config.showDescriptions = not config.showDescriptions
     pd.datastore.write(config)
@@ -64,11 +64,15 @@ function startGame()
   -- globally track crank position
   crankTicks = 0
 
+  -- get all sounds in memory
   sounds = {
     flip = pd.sound.sampleplayer.new("sounds/flip.wav"),
     match = pd.sound.sampleplayer.new("sounds/match.wav"),
     mismatch = pd.sound.sampleplayer.new("sounds/mismatch.wav")
   }
+
+  -- track the most recent sequence of keys pressed, for secrets!
+  keyBuffer = {}
 
   setupBoard()
   setupSelector()
@@ -83,8 +87,20 @@ function restartGame()
 end
 
 function setupBoard()
-  local card = Card('', 0, 0, 0, 0)
+  -- draw background
+  local bgImage <const> = gfx.image.new("images/bg")
+  gfx.sprite.setBackgroundDrawingCallback(
+    function(x, y, width, height)
+      gfx.setClipRect(x, y, width, height)
+      bgImage:draw(0, 0)
+      gfx.clearClipRect()
+    end
+  )
 
+  -- sample card so we can get its size
+  local card <const> = Card('', 0, 0, 0, 0)
+
+  -- place cards on the board
   for i=1,board.cols do
     cards[i] = {}
     for j=1,board.rows do
@@ -101,8 +117,8 @@ end
 -- It's responsible for moving itself around the screen when you use the d-pad
 -- or crank. What happens when you press A on a card is handled in update() below
 function setupSelector()
-  local card = Card('', 0, 0, 0, 0)
-  local options = {
+  local card <const> = Card('', 0, 0, 0, 0)
+  local options <const> = {
     cols = board.cols,
     rows = board.rows,
     colWidth = card.width,
@@ -121,9 +137,43 @@ function setupScoreboard()
   scoreboard = Scoreboard(302, 0, labels)
 end
 
+-- flips all cards to either their front or back sides
+function flipAllCards(side)
+  for i=1,board.cols do
+    for j=1,board.rows do
+      cards[i][j]:flip(side)
+    end
+  end
+end
+
+-- records a key press, only tracking the last 10 keys
+function recordKey(key)
+  table.insert(keyBuffer, #keyBuffer + 1, key)
+  if #keyBuffer > 10 then
+    table.remove(keyBuffer, 1)
+  end
+end
+
+-- if two mismatched cards are showm, as soon as a button is pressed or the
+-- crank turns, flip them back over
+function handleMismatch()
+  if not mismatch then
+    return
+  end
+
+  if pd.buttonJustPressed(pd.kButtonUp) or
+     pd.buttonJustPressed(pd.kButtonDown) or
+     pd.buttonJustPressed(pd.kButtonLeft) or
+     pd.buttonJustPressed(pd.kButtonRight) or
+     pd.buttonJustPressed(pd.kButtonA) or
+     crankTicks ~= 0 then
+       flipAllCards('back')
+       mismatch = false
+  end
+end
+
 -- What to do when pressing the A button
 function handleA()
-
   -- if a timer is already running don't let any other A button actions take place
   if matchTimer then
     return
@@ -133,9 +183,19 @@ function handleA()
   -- unless the card to flip is one of the mismatched ones, then it should show
 
   if pd.buttonJustPressed(pd.kButtonA) then
-    local selected = selector:which()
-    cards[selected.col][selected.row]:flip()
+    recordKey('a')
 
+    -- flip over the card that's currently selected, if visible
+    local selected = selector:which()
+    if cards[selected.col][selected.row].visible then
+      cards[selected.col][selected.row]:flip()
+    else
+      -- card is already matched and off the table
+      selector:shake('left-right')
+      return
+    end
+
+    -- record all cards currently visible
     local showing = {}
     for i=1,board.cols do
       for j=1,board.rows do
@@ -148,25 +208,21 @@ function handleA()
     -- are there exactly two cards visible?
     if (#showing == 2) then
       if showing[1].label == showing[2].label then
+        -- we have a match :)
         scoreboard:update(showing[1].label)
         sounds.match:play()
-        -- two matching cards are showing, hide them after a delay
         matchTimer = pd.timer.performAfterDelay(1000, function()
           showing[1]:remove()
           showing[2]:remove()
           matchTimer = nil
         end)
       else
+        -- not a match :(
         selector:shake('left-right')
         sounds.mismatch:play()
-        -- when the cursor next moves, flip the cards back over
+        -- keep track so that when the cursor next moves or button pressed,
+        -- we'll know to flip the cards back over
         mismatch = true
-        -- two mismatched cards are showing, flip them back over after a delay
-        -- matchTimer = pd.timer.performAfterDelay(500, function()
-        --   showing[1]:flip()
-        --   showing[2]:flip()
-        --   matchTimer = nil
-        -- end)
       end
     else
       -- if we're not going to play any other sound, play the card flip
@@ -178,46 +234,41 @@ end
 -- What to do when pressing the B button
 function handleB()
   if pd.buttonJustPressed(pd.kButtonB) then
-    print('B pressed')
+    recordKey('b')
+  end
+end
+
+function handleD()
+  if pd.buttonJustPressed(pd.kButtonUp) then
+    recordKey('u')
+  end
+  if pd.buttonJustPressed(pd.kButtonDown) then
+    recordKey('d')
+  end
+  if pd.buttonJustPressed(pd.kButtonLeft) then
+    recordKey('l')
+  end
+  if pd.buttonJustPressed(pd.kButtonRight) then
+    recordKey('r')
   end
 end
 
 function pd.update()
-  handleA()
-  handleB()
-
+  -- crankTicks can only be called one per update loop so do it here for
+  -- everyone to use the value
   crankTicks = pd.getCrankTicks(12)
 
+  handleMismatch()
+
+  -- watch for inputs
+  handleA()
+  handleB()
+  handleD()
+
+  -- draw everything
   gfx.sprite.update()
   pd.timer.updateTimers()
-
-  if mismatch and (
-     pd.buttonJustPressed(pd.kButtonUp) or
-     pd.buttonJustPressed(pd.kButtonDown) or
-     pd.buttonJustPressed(pd.kButtonLeft) or
-     pd.buttonJustPressed(pd.kButtonRight) or
-     crankTicks ~= 0) then
-       flipAllCards('back')
-       mismatch = false
-  end
 end
-
-function flipAllCards(side)
-  for i=1,board.cols do
-    for j=1,board.rows do
-      cards[i][j]:flip(side)
-    end
-  end
-end
-
-local bgImage = gfx.image.new("images/bg")
-gfx.sprite.setBackgroundDrawingCallback(
-  function(x, y, width, height)
-    gfx.setClipRect(x, y, width, height)
-    bgImage:draw(0, 0)
-    gfx.clearClipRect()
-  end
-)
 
 setupMenu()
 startGame()
